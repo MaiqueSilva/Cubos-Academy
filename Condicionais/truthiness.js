f16d719cfed357f09b358d14560d3fbfdb5d0ed9
Maique Almeida da Silva <maique.almeida@hotmail.com>
2022-09-09 17:26:09 -0300

    conteudo adicionado

---
+++ Condicionais/truthiness.js
@@ -0,0 +1,11 @@
+//basicamente uma definição do JS, Pois js sempre da um jeito para executar alguma coisa
+
+const x = ""
+
+if (x) {
+    console.log("Truthy")
+} else {
+    console.log("Falsy")
+}
+
+//https://developer.mozilla.org/pt-BR/docs/Glossary/Truthy
