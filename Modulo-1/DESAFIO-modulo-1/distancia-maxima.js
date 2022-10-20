070cd3555c2020d137c10a0882d9acfcd7e43c1c
Maique Almeida da Silva <maique.almeida@hotmail.com>
2022-10-19 22:16:44 -0300

    exercicio concluido

--- Modulo-1/DESAFIO-modulo-1/distancia-maxima.js
+++ Modulo-1/DESAFIO-modulo-1/distancia-maxima.js
@@ -1,38 +1,39 @@
-const coordenadas = [{ x: 0, y: 0 }, { x: 0, y: 3 }, { x: 4, y: 0 }]
+function processData(input) {
+    let linhas = input.trim().split("\n");
+    let coordenadas = [];
 
-
-// const linhas = input.trim().split("\n");
-// const n = parseInt(linhas[0]);
-
-// for (let i = 1; i < linhas.length; i++) {
-//     const coord = linhas[i].split(" ");
-//     coordenadas.push(
-//         {
-//             x: parseInt(coord[0]), //transformar string em numero decimal
-//             y: parseInt(coord[1], 10), //mesmo coisa de cima.
-//         }
-//     )
-// }
-
-let valorMaximo = 0
-
-for (let i = 0; i < coordenadas.length; i++) {
-    let x1 = coordenadas[i].x;
-    let y1 = coordenadas[i].y;
-    let x2 = coordenadas[i++].x;
-    let y2 = coordenadas[i++].y;
-
-
-    let calculo = Math.sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
-    if (calculo >= valorMaximo) {
-        valorMaximo = calculo
+    for (const linha of linhas) {
+        let coordenadasXY = linha.split(" ")
+        coordenadas.push({
+            x: coordenadasXY[0],
+            y: coordenadasXY[1]
+        });
     }
 
+
+    let x1
+    let y1
+    let x2
+    let y2
+
+    let valorMaximo = 0
+
+    for (let i = 0; i < coordenadas.length; i++) {
+
+        for (let j = 1; j < coordenadas.length; j++) {
+            let x1 = coordenadas[i].x;
+            let y1 = coordenadas[i].y;
+            let x2 = coordenadas[j].x;
+            let y2 = coordenadas[j].y;
+
+            let calculo = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+            if (calculo >= valorMaximo) {
+                valorMaximo = calculo
+            }
+
+        }
+    }
 }
-
-console.log(valorMaximo)
-
-
 
 
 
--- Modulo-1/DESAFIO-modulo-1/teste3.js
+++ Modulo-1/DESAFIO-modulo-1/teste3.js
@@ -1,36 +1,36 @@
-const coordenadas = [{ x: 0, y: 0 }, { x: 0, y: 3 }, { x: 4, y: 0 }]
+function processData(input) {
+    let linhas = input.trim().split("\n");
+    let coordenadas = [];
 
-let index1 = 0
-let index2 = 1
-let valorMaximo = 0
-let contagem = 0
-
-for (let i = 0; i < coordenadas.length; i++) {
-    let x1 = coordenadas[index1].x;
-    let y1 = coordenadas[index1].y;
-    let x2 = coordenadas[index2].x;
-    let y2 = coordenadas[index2].y;
-    index2++;
-
-    if (i === coordenadas.length || index2 === coordenadas.length) {
-        index1++;
-        index2 = 0;
-        i = -1;
-    }
-    //  else if (i > coordenadas.length || i < coordenadas.length) {
-    //     index2++;
-    // }
-
-
-
-
-    let calculo = Math.sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
-    if (calculo >= valorMaximo) {
-        valorMaximo = calculo
+    for (const linha of linhas) {
+        let coordenadasXY = linha.split(" ")
+        coordenadas.push({
+            x: coordenadasXY[0],
+            y: coordenadasXY[1]
+        });
     }
 
-    contagem++
+
+    let x1
+    let y1
+    let x2
+    let y2
+
+    let valorMaximo = 0
+
+    for (let i = 0; i < coordenadas.length; i++) {
+
+        for (let j = 1; j < coordenadas.length; j++) {
+            let x1 = coordenadas[i].x;
+            let y1 = coordenadas[i].y;
+            let x2 = coordenadas[j].x;
+            let y2 = coordenadas[j].y;
+
+            let calculo = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+            if (calculo >= valorMaximo) {
+                valorMaximo = calculo
+            }
+
+        }
+    }
 }
-
-console.log(valorMaximo)
-console.log(contagem)
